195ad9e6f0bdba5040ed7496b76da1bad0e400bd
mpiquero <mpiquero7164@gmail.com>
2017-05-26 11:09:51 -0400

    init

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = {app};
