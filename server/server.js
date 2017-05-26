a39664e6ef26a434598c2f47d03376853ab8cf3f
mpiquero <mpiquero7164@gmail.com>
2017-05-26 14:34:30 -0400

    added js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,51 @@
+//  https://warm-stream-20676.herokuapp.com/
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-module.exports = {app};
+server.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
